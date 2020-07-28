1daadeabf408d2c82807e8db279f0892d43ae835
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-07-27 22:14:33 -0300

    Some adjustments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
+const BOX_SIZE = 20;
+const BOX_COUNT = 30;
+const DIRECTION_RIGHT = 'right';
+const DIRECTION_LEFT = 'left';
+const DIRECTION_UP = 'up';
+const DIRECTION_DOWN = 'down';
+const KEY_RIGHT = 39;
+const KEY_LEFT = 37;
+const KEY_UP = 38;
+const KEY_DOWN = 40;
+
 const canvas = document.getElementById('app'),
-    context = canvas.getContext('2d'),
-    boxSize = 20,
-    boxCount = 30;
+    context = canvas.getContext('2d');
 
 let direction = null,
     food = null,
@@ -9,54 +18,54 @@ let direction = null,
     game = null;
 
 function moveSnake (event) {
-    if (event.keyCode == 37 && direction != 'right') {
-        direction = 'left';
+    if (event.keyCode === KEY_LEFT && direction !== DIRECTION_RIGHT) {
+        direction = DIRECTION_LEFT;
     }
 
-    if (event.keyCode == 38 && direction != 'down') {
-        direction = 'up';
+    if (event.keyCode === KEY_UP && direction !== DIRECTION_DOWN) {
+        direction = DIRECTION_UP;
     }
 
-    if (event.keyCode == 39 && direction != 'left') {
-        direction = 'right';
+    if (event.keyCode === KEY_RIGHT && direction != DIRECTION_LEFT) {
+        direction = DIRECTION_RIGHT;
     }
 
-    if (event.keyCode == 40 && direction != 'up') {
-        direction = 'down';
+    if (event.keyCode === KEY_DOWN && direction != DIRECTION_UP) {
+        direction = DIRECTION_DOWN;
     }
 }
 
 function drawSnake () {
     snake.forEach(part => {
         context.fillStyle = "lightgreen";
-        context.fillRect(part.x, part.y, boxSize, boxSize);
+        context.fillRect(part.x, part.y, BOX_SIZE, BOX_SIZE);
     })
 };
 
 function drawFood () {
     context.fillStyle = "red";
-    context.fillRect(food.x, food.y, boxSize, boxSize);
+    context.fillRect(food.x, food.y, BOX_SIZE, BOX_SIZE);
 };
 
 function clearCanvas(){
     context.fillStyle = "black";
-    context.fillRect(0, 0, boxCount * boxSize, boxCount * boxSize);
+    context.fillRect(0, 0, BOX_COUNT * BOX_SIZE, BOX_COUNT * BOX_SIZE);
 };
 
 function isGameOver () {
-    if (snake[0].x > (boxCount - 1) * boxSize && direction === "right") {
+    if (snake[0].x > (BOX_COUNT - 1) * BOX_SIZE && direction === DIRECTION_RIGHT) {
         return true;
     }
 
-    if (snake[0].x < 0 && direction === 'left') {
+    if (snake[0].x < 0 && direction === DIRECTION_LEFT) {
         return true;
     }
 
-    if (snake[0].y > (boxCount - 1) * boxSize && direction === "down") {
+    if (snake[0].y > (BOX_COUNT - 1) * BOX_SIZE && direction === DIRECTION_DOWN) {
         return true;
     }
 
-    if (snake[0].y < 0 && direction === 'up') {
+    if (snake[0].y < 0 && direction === DIRECTION_UP) {
         return true;
     }
 
@@ -84,41 +93,41 @@ function play () {
         ...snake[0],
     };
 
-    if (direction === "right") {
-        newHead.x += boxSize;
+    if (direction === DIRECTION_RIGHT) {
+        newHead.x += BOX_SIZE;
     }
 
-    if (direction === "left") {
-        newHead.x -= boxSize;
+    if (direction === DIRECTION_LEFT) {
+        newHead.x -= BOX_SIZE;
     }
 
-    if (direction === "up") {
-        newHead.y -= boxSize;
+    if (direction === DIRECTION_UP) {
+        newHead.y -= BOX_SIZE;
     }
 
-    if (direction === "down") {
-        newHead.y += boxSize;
+    if (direction === DIRECTION_DOWN) {
+        newHead.y += BOX_SIZE;
     }
 
     snake.unshift(newHead);
 
     if (newHead.x === food.x && newHead.y === food.y) {
-        food.x = Math.floor(Math.random() * (boxCount - 1) +1) * boxSize;
-        food.y = Math.floor(Math.random() * (boxCount - 1) +1) * boxSize;
+        food.x = Math.floor(Math.random() * (BOX_COUNT - 1) +1) * BOX_SIZE;
+        food.y = Math.floor(Math.random() * (BOX_COUNT - 1) +1) * BOX_SIZE;
     } else {
         snake.pop();
     }
 };
 
 function start () {
-    direction = 'right';
+    direction = DIRECTION_RIGHT;
     food = {
-        x: Math.floor(Math.random() * (boxCount - 1) + 1) * boxSize,
-        y: Math.floor(Math.random() * (boxCount - 1) + 1) * boxSize
+        x: Math.floor(Math.random() * (BOX_COUNT - 1) + 1) * BOX_SIZE,
+        y: Math.floor(Math.random() * (BOX_COUNT - 1) + 1) * BOX_SIZE
     };
     snake = [{
-        x: (boxCount / 2) * boxSize,
-        y: (boxCount / 2) * boxSize,
+        x: (BOX_COUNT / 2) * BOX_SIZE,
+        y: (BOX_COUNT / 2) * BOX_SIZE,
     }];
     game = setInterval(play, 100);
 };
