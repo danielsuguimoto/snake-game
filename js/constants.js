9c137c4a2a00e1781e2c3499f491958c664bb2c3
Daniel Jun Suguimoto <danielsuguimoto@gmail.com>
2020-07-28 20:29:14 -0300

    Removed unused file

--- js/constants.js
+++
@@ -1,8 +0,0 @@
-export const DIRECTION_RIGHT = 1;
-export const DIRECTION_LEFT = 2;
-export const DIRECTION_UP = 4;
-export const DIRECTION_DOWN = 8;
-export const KEY_RIGHT = 39;
-export const KEY_LEFT = 37;
-export const KEY_UP = 38;
-export const KEY_DOWN = 40;
